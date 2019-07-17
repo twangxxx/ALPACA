2a377978133fa0d214b36ecedb30a7562bede2a2
TianWang <tianwangxxx@gmail.com>
2019-07-17 16:51:28 -0400

    edit .travis.yml

--- travis_script.r
+++ travis_script.r
@@ -1,5 +1,5 @@
 install.packages("devtools")
 library(devtools)
 
-# update.packages(ask = FALSE, checkBuilt = TRUE)
+update.packages(ask = FALSE, checkBuilt = TRUE)
 devtools::install_deps(dep = T)
